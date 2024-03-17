ddad69a4c1645f90f19f5ef4aa67338ae3e6726a
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-03-17 13:24:23 +0300

    1

--- app.js
+++ app.js
@@ -13,12 +13,54 @@
 // let result = tagQosh();
 // console.log(result);
 //101-masala
+let raqam = +prompt("raqam kiriting");
+for (i = raqam; i > 0; i--) {
+  let son1 = 0;
+  let son = son1 + i;
+  let result = son;
+  console.log(result);
+}
+
 // 102-masala
 // 103-masala
 // 104-masala
 // 105-masala
+// let arr = ["fanta", "cola", "water"];
+// let suv = cola,
+//   fanta;
+// function tekshir(arr) {
+//   let newArr = [];
+//   for (i = 0; i < arr.length; i++) {
+//     if (i == suv || i == suv) {
+//     } else {
+//       newArr.push();
+//       return newArr;
+//     }
+//   }
+// }
+// console.log(tekshir(arr));
+// let arr = ["fanta", "cola", "water"];
+// let suv = cola,
+//   fanta;
+// function tekshir(arr) {
+//   let newArr = [];
+//   for (i = 0; i < arr.length; i++) {
+//     if (i == suv || i == suv) {
+//     } else {
+//       newArr.push();
+//       return newArr;
+//     }
+//   }
+// }
+// console.log(tekshir(arr));
 // 106-masala
+// let son = prompt("son kiriting");
+// console.log(son.length);
 // 107-masala
 // 108-masala
 // 109-masala
 // 110-masala
+// let ism = prompt("ismingizni yozing");
+// let boshharif = ism.charAt(0).toUpperCase();
+// let qolganharf = ism.slice(1).toLowerCase();
+// alert(`Salom ${boshharif}${qolganharf}`);
