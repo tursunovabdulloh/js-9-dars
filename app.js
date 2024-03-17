235c345b3b0b7d5bb25f21d9c9ec9b44fbb124c6
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-03-17 03:39:02 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+// 100-masala
+// Namuna("div*2") ➞ "<div></div><div></div>"
+// function tagQosh(tag, son) {
+//   tag = prompt("tag kiriting");
+//   son = +prompt("son kiriting");
+//   let yozuv = "";
+//   for (i = 1; (i = son); i++) {
+//     yozuv = `${yozuv} + <>tag</>`;
+//   }
+//   let natija = `${tag} * ${son} = }`;
+//   return natija;
+// }
+// let result = tagQosh();
+// console.log(result);
+//101-masala
+// 102-masala
+// 103-masala
+// 104-masala
+// 105-masala
+// 106-masala
+// 107-masala
+// 108-masala
+// 109-masala
+// 110-masala
