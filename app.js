2c47cdd690e6d1ea06a844d289f9bc973f5bcd45
Abdullohtursunov <tursunovabdulloh172@gmail.com>
2024-03-18 09:05:56 +0300

    1

--- app.js
+++ app.js
@@ -46,8 +46,8 @@
 // }
 // console.log(testkvadrat());
 // 104-masala
-let arr = [0, 0, 0, 0, 0, 0];
-console.log(arr);
+// let arr = [0, 0, 0, 0, 0, 0];
+// console.log(arr);
 // 105-masala
 // let arr = ["fanta", "cola", "water"];
 // let suv = cola,
@@ -81,10 +81,33 @@ console.log(arr);
 // let son = prompt("son kiriting");
 // console.log(son.length);
 // 107-masala
+
+// function hisobla(son = +prompt) {
+//   let son = 123;
+//   let birinchi_son = son % 100;
+//   let ikkinchi_son = son % 10;
+//   let uchinchi_son = son / 100;
+// }
 // 108-masala
+function getRandomInt(son) {
+  return Math.ceil(Math.random() * son);
+}
+console.log(getRandomInt(5));
+console.log(getRandomInt(9));
 // 109-masala
 // 110-masala
 // let ism = prompt("ismingizni yozing");
 // let boshharif = ism.charAt(0).toUpperCase();
 // let qolganharf = ism.slice(1).toLowerCase();
 // alert(`Salom ${boshharif}${qolganharf}`);
+// function updateBrand(obj) {
+//   obj.brand = "Toyota";
+// }
+// const car = {
+//   brand: "Honda",
+//   model: "Accord",
+//   year: 1998,
+// };
+// console.log(car.brand);
+// updateBrand(car);
+// console.log(car.year);
